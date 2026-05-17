9af317c58823a057ec6447ddd911046a1557c505
nguyenthinh28902 <nndthinh28902@gmail.com>
2026-05-17 18:25:16 +0700

    first commit

---
+++ Library/FollowMeProject.Library.Core/Abstractions/Persistence/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FollowMeProject.Library.Core.Abstractions.Persistence
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> Entities { get; }
+        IQueryable<T> EntitiesNoTracking { get; }
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        public void Detached(T entity);
+    }
+
+}
